b2883127d0919e0542add534e38ecf30b619ef0c
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-20 17:28:32 +0300

    Header and home use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Login from "./components/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Header />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import Header from "./Header";
 
 const Home = () => {
   return (
     <div>
-      
+      <Header />
+      home
     </div>
   );
 };
-
 
 export default Home;
