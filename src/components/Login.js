a540dd0f5f117069a444eebbf0db5a6ff9c032d9
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-20 11:02:48 +0300

    router

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
 const Login = (props) => {
-  return <div> Login</div>;
+  return (
+    <Container>
+      <Nav>
+        <a href="#">
+          <img src="/image logo/image.png" alt="image" />
+        </a>
+      </Nav>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+const Nav = styled.nav``;
 
 export default Login;
