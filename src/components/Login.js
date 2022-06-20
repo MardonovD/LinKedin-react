6c6024d6fccf45a5767cfe7be4e3f6916da0d4ab
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-20 11:57:12 +0300

    Sign In btn

--- src/components/Login.js
+++ src/components/Login.js
@@ -61,7 +61,10 @@ const SignIn = styled.a`
   box-shadow: inset 0 0 0 1px #0a66c2;
   color: #0a66c2;
   border-radius: 24px;
-  
+  transition-duration: 200ms;
+  font-weight: 600;
+  font-size: 25px;
+  padding: 10px 24px;
 `;
 
 export default Login;
