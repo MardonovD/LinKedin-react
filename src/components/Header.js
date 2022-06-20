8ea96f90903fe54d4d7a58ff676e6d4a6efe9b18
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-20 17:32:32 +0300

    Header styled Arxitektura

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Content>Header</Content>
+    </Container>
+  );
 };
 
+const Container = styled.div`
+
+
+`;
+const Content = styled.div`
+
+`;
 export default Header;
