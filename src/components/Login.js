25fd17ae2b2edc288772dd18422b5aebdfd30ccb
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-20 12:35:36 +0300

    Hero main page

--- src/components/Login.js
+++ src/components/Login.js
@@ -103,6 +103,15 @@ const Hero = styled.div`
     padding-bottom: 0;
     width: 55%;
     font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      font-size: 20px;
+      text-align: center;
+      width: 100%;
+      line-height: 2;
+    }
   }
 `;
 export default Login;
