d25d995a7837589ce63a9e17e36370a1a97a7431
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 10:05:28 +0300

    Photo my image

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -51,6 +51,7 @@ const CardBackground = styled.div`
 `;
 const Photo = styled.div`
   box-shadow: none;
+  background-image: url("/image logo/myImage.png");
 `;
 const Link = styled.div``;
 const AddPhotoText = styled.div``;
