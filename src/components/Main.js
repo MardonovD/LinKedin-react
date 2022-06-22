75a4a12e4c45303db833ea6528fdcf1bcc132f74
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 09:05:49 +0300

    Main side container

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 const Main = () => {
-  return (
-
-    
-  )
+  return <Container>Main side</Container>;
 };
 
 export default Main;
