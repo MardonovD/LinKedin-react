92d592ee8d6fbecbbade0ebd9fe9e50475bbec50
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 08:48:51 +0300

    media query column main page

--- src/components/Home.js
+++ src/components/Home.js
@@ -59,6 +59,14 @@ const Layout = styled.div`
   display: grid;
   grid-template-areas: " leftSide  middle side Right Side";
   grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  grid-template-rows: auto;
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export default Home;
