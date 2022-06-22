825af9d9307b43f8e6bd743ead25bbaab235ddd8
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 09:09:15 +0300

    Right side

--- src/components/RoghtSide.js
+++ src/components/RoghtSide.js
@@ -2,9 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 const RoghtSide = () => {
   return (
-    <div>
+    <Container>
+
       
-    </div>
+    </Container>
   );
 };
 
