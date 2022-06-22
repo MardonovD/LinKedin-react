66ab2186dd220d4be80d86e5526daa347fa7e714
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 09:45:56 +0300

    reading

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -35,7 +35,9 @@ const ArtCard = styled.div`
   box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 /20%);
 `;
 
-const UserInfo = styled.div``;
+const UserInfo = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+`;
 
 const CardBackground = styled.div``;
 const Photo = styled.div``;
