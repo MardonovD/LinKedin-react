b3871f32ea689ec7370731cd79867342e20b2656
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 08:18:48 +0300

    header browserRouter

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <Container>
       <Section>
         <h5>
-          <a href="#"> Hiring in a hurry? -</a>
+          <a> Hiring in a hurry? -</a>
         </h5>
         <p>
           Find talented pros in record time with Upwork and keep business moving
@@ -21,10 +21,26 @@ const Container = styled.div`
   padding-top: 70px;
   max-width: 100%;
 `;
+
 const Section = styled.section`
   min-height: 50px;
   padding: 16px 0;
   text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-weight: 700;
+    font-size: 14px;
+    color: #434649;
+  }
 `;
 
 export default Home;
