e401a8a1232066ef49c4dec79f25fb96ee43de35
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 09:06:55 +0300

    Grid area

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 const Main = () => {
   return <Container>Main side</Container>;
 };
-const Container = styled.div``;
+const Container = styled.div`
+
+grid-area: ;
+`;
 
 export default Main;
