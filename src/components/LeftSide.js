c8aedcc2966d6212c02bbd9ccb10ec149d4c0f48
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 09:03:26 +0300

    Container

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 const LeftSide = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <Container>Left side</Container>;
 };
-
 
 export default LeftSide;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 const Main = () => {
-  return <div></div>;
+  return (
+
+    
+  )
 };
 
 export default Main;
