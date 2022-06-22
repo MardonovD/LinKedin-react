f67aa503a093fd4ab18b6a3a4ce24907941796f7
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-22 09:01:17 +0300

    Main side styled import

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,7 @@
-import React from 'react';
-
+import React from "react";
+import styled from "styled-components";
 const Main = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 };
-
 
 export default Main;
